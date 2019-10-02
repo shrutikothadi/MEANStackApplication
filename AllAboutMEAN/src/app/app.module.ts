028c045d34896176047a589a48093edd554204fc
shrutikothadi <shrutikothadi395@gmail.com>
2019-10-02 18:16:30 +0530

    NAVBAR
    NAVBAR

--- AllAboutMEAN/src/app/app.module.ts
+++ AllAboutMEAN/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProductsComponent } from './products/products.component';
 import {  HttpClientModule } from '@angular/common/http';
 import { AgmCoreModule } from '@agm/core';
 import { AngularRaveModule } from 'angular-rave';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
